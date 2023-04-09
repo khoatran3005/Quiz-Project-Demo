40b178e9bebe0510466cb9aafc9bd305a4e2e26b
Khoa Tran <khoatran3005@gmail.com>
2023-04-09 02:40:40 -0400

    video16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    render() {
+        return (
+
+            <div> My First Component 
+                {}
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
