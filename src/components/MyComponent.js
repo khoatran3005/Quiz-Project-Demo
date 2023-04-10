e22587202ebe77eff5f6b326d23b36d92d89518a
Khoa Tran <khoatran3005@gmail.com>
2023-04-09 21:52:34 -0400

    event

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,26 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+    state = {
+        name: 'Khoa Tran',
+        address: 'Atlanta',
+        age: 21
+    }
+
+    handleClick(_event) {
+        console.log('My name is', this.state.name);
+    }
+
+    handleOnMouseOver(event) {
+        console.log(event.clientX)
+    }
+
     render() {
         return (
 
-            <div> My First Component 
-                {}
+            <div> My First Component
+                <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
+                <button onClick={this.handleClick}>Click me</button>
             </div>
         );
     }
