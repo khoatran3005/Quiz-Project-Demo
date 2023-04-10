2194d2531f9663cc600afa349e5ea39b5aec6d43
Khoa Tran <khoatran3005@gmail.com>
2023-04-10 04:13:09 -0400

    add props

--- src/components/AddUserInfor.js
+++ src/components/AddUserInfor.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-class UserInfor extends React.Component {
+class AddUserInfor extends React.Component {
     state = {
-        name: 'Khoa Tran',
-        address: 'Atlanta',
-        age: 21
+        name: '',
+        address: '',
+        age:''
     }
 
     handleOnChangeInput = (event) => {
@@ -22,6 +22,11 @@ class UserInfor extends React.Component {
     handleOnSubmit = (event) => {
         event.preventDefault();
         console.log(this.state)
+        this.props.handleAddNewUser({
+            id: Math.floor(Math.random() * 100) + 1 + '-random',
+            name:this.state.name,
+            age:this.state.age
+        });
     }
 
 
@@ -50,4 +55,4 @@ class UserInfor extends React.Component {
     }
 }
 
-export default UserInfor;
+export default AddUserInfor;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
-import UserInfor from "./UserInfor";
+import AddUserInfor from "./AddUserInfor";
 import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
@@ -12,13 +12,23 @@ class MyComponent extends React.Component {
         ]
     }
 
+    handleAddNewUser = (userObj) =>{
+
+        
+        this.setState({
+            listUsers:[userObj, ...this.state.listUsers]
+        })
+    }
+
     render() {
         return (
             <div> 
-                <UserInfor></UserInfor>
+                <AddUserInfor handleAddNewUser={this.handleAddNewUser}></AddUserInfor>
                 <br/><br/>
                 <DisplayInfor 
-                    listUsers={this.state.listUsers}></DisplayInfor>
+                    listUsers={this.state.listUsers}
+                    >
+                </DisplayInfor>
             </div>
         );
     }
