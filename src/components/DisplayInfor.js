14b90772cd13e67e625d32350ba3f9d3dc7e26a8
Khoa Tran <khoatran3005@gmail.com>
2023-04-11 20:39:23 -0400

    hook

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,43 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './DisplayInfor.scss';
 import logo from './../logo.svg';
-
-// class DisplayInfor extends React.Component {
-//     constructor(props){
-//         console.log('call constructor: 1')
-//         super(props);
-//         this.state = {
-//             isShowListUser: true,
-//         }
-//     }
-
-//     render() {
-//         console.log('call me render')
-//         const {listUsers} = this.props;
-//         return (
-//             <div className="display-infor-container">
-//                 { true && 
-//                 <>
-//                     {listUsers.map((user, index) => {
-//                             return (
-//                                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-//                                     <div >My name is {user.name}.</div>
-//                                     <div>My age is {user.age}.</div>
-//                                     <div>
-//                                         <button onClick={() => this.props.handleDeleteUser(user.id)}>
-//                                             Delete
-//                                         </button>
-//                                     </div>
-//                                     <hr/>
-//                                 </div>
-//                             )
-//                     })}
-//                 </>
-//                 }
-//             </div>
-//         );
-//     }
-// }
 
 const DisplayInfor = (props) => {
     const { listUsers } = props;
@@ -47,6 +10,11 @@ const DisplayInfor = (props) => {
     const handleShowHideListUser = () => {
         setShowHideListUser(!isShowHideListUser)
     }
+
+    useEffect(() =>{
+        console.log('me')
+    },[listUsers]
+    )
 
     return (
         <div className="display-infor-container">
