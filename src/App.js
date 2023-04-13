a3bbc1587ef6e8057fbea67838d518e15ad622c0
Khoa Tran <khoatran3005@gmail.com>
2023-04-13 14:25:44 -0400

    #46

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
-import logo from './logo.svg';
 import './App.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-import MyComponent from './components/MyComponent';
-import React from 'react';
 
-class App extends React.Component {
+const App = () => {
 
-  state = {
-    name: 'Khoa Tran',
-    address: 'Khoa Hoc Gioi Nhat',
-    age: 21
-  }
-
-
-  render() {
-    return (
-      <div className='app-container'>
-        <MyComponent></MyComponent>
-      </div>
-    );
-  }
+  return (
+    <div className="app-container">
+      Hello world
+    </div>
+  );
 }
-
-
-// const App = () => {
-//   const count = useSelector(state => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Hello world with Khoa
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//   );
-// }
 export default App;
