81d5ad786995c41e2fd1831e8b85689d9fccb90d
Khoa Tran <khoatran3005@gmail.com>
2023-04-16 17:58:20 -0400

    bai 70

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -1,10 +1,9 @@
-import axios from 'axios';
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { FcPlus } from 'react-icons/fc';
 import { toast } from 'react-toastify';
-
+import { postCreateNewUser } from '../../../service/apiService';
 
 const ModalCreateUser = (props) => {
     const { show, setShow } = props;
@@ -34,25 +33,25 @@ const ModalCreateUser = (props) => {
 
     const validateEmail = (email) => {
         return String(email)
-          .toLowerCase()
-          .match(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          );
-      };
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
 
-    const handleSubmitCreateUser = async() => {
-        
+    const handleSubmitCreateUser = async () => {
+
         const isValidEmail = validateEmail(email);
         if (!isValidEmail) {
             toast.error("Invalid email");
             return;
         }
-        
+
         if (!password) {
             toast.error("Invalid password");
             return;
         }
-        
+
         // let data = {
         //     email: email,
         //     password: password,
@@ -62,21 +61,13 @@ const ModalCreateUser = (props) => {
         // }
         // console.log(data)
 
-        const form = new FormData();
-        form.append('email', email);
-        form.append('password', password);
-        form.append('username', username);
-        form.append('role', role);
-        form.append('userImage', image);
-
-        let res = await axios.post('http://localhost:8081/api/v1/participant', form)
-        console.log(res.data)
-        if(res.data && res.data.EC === 0) {
-            toast.success(res.data.EM);
+        let data = await postCreateNewUser(email, password, username, role, image)
+        if (data && data.EC === 0) {
+            toast.success(data.EM);
             handleClose();
         }
 
-        if(res.data && res.data.EC !== 0) {
+        if (data && data.EC !== 0) {
             toast.error("The email is already exis");
         }
     }
