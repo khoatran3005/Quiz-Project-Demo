b5b72c204c40fac7ccc9702c0ff3a48e762d328d
Khoa Tran <khoatran3005@gmail.com>
2023-04-24 00:02:11 -0400

    api new quiz

--- src/components/Admin/Content/Quiz/ManageQuiz.js
+++ src/components/Admin/Content/Quiz/ManageQuiz.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import './ManageQuiz.scss';
 import Select from 'react-select';
+import { postCreateNewQuiz } from '../../../../service/apiService'
+import { toast } from 'react-toastify';
 
 const options = [
     { value: 'EASY', label: 'EASY' },
@@ -10,65 +12,87 @@ const options = [
 
 const ManageQuiz = (props) => {
 
-    const [name,setName] =useState('');
-    const [description,setDescription] =useState('');
-    const [type,setType] =useState('EASY');
-    const [image,setimage] =useState(null);
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [type, setType] = useState('EASY');
+    const [image, setImage] = useState('');
 
-    const handleChangeFile = () =>{
-
+    const handleChangeFile = (event) => {
+        if (event.target && event.target.files && event.target.files[0]) {
+            setImage(event.target.files[0])
+        }
     }
 
-    return (
-        <div className="quiz-container">
-            <div className="title">
-                ManageQuiz
+    const handleSubmitQuiz = async () => {
+        if (!name || !description) {
+            toast.error('Name/Description is required')
+            return;
+        }
+
+        let res = await postCreateNewQuiz(description, name, type?.value, image);
+        if (res && res.EC === 0){
+            toast.success(res.EM)
+            setName('');
+            setDescription('');
+            setImage(null);
+        } else {
+            toast.error(res.EM)
+        }
+    }
+        return (
+            <div className="quiz-container">
+                <div className="title">
+                    ManageQuiz
+                </div>
+                <div className="add-new">
+                    <fieldset class="border rounded-3 p-3">
+                        <legend class="float-none w-auto px-3">Add new quiz:</legend>
+                        <div class="form-floating mb-3">
+                            <input
+                                type="text"
+                                class="form-control"
+                                placeholder='your-quiz-name'
+                                value={name}
+                                onChange={(event) => setName(event.target.value)} />
+                            <label for="floatingInput">Name</label>
+                        </div>
+                        <div class="form-floating">
+                            <input
+                                type="text"
+                                class="form-control"
+                                placeholder='description'
+                                value={description}
+                                onChange={(event) => setDescription(event.target.value)} />
+                            <label for="floatingPassword">Description</label>
+                        </div>
+                        <div className='my-3'>
+                            <Select
+                                defaultValue={type}
+                                onChange={setType}
+                                // onChange={this.handleChange}
+                                options={options}
+                                placeholder={'Quiz type ...'}
+                            />
+                        </div>
+                        <div className='more-actions form-group'>
+                            <label className='mb-1'>Upload Image</label>
+                            <input
+                                type='file'
+                                className='form-control'
+                                onChange={(event) => handleChangeFile(event)} />
+                        </div>
+                        <div className='mt-3'>
+                            <button
+                                className='btn btn-warning '
+                                onClick={() => handleSubmitQuiz()}>Save</button>
+                        </div>
+                    </fieldset>
+                </div>
+                <div className="list-detail">
+
+                </div>
             </div>
-            <div className="add-new">
+        )
+    }
 
-
-                <fieldset class="border rounded-3 p-3">
-                    <legend class="float-none w-auto px-3">Add new quiz:</legend>
-                    <div class="form-floating mb-3">
-                        <input 
-                        type="text" 
-                        class="form-control" 
-                        placeholder='your-quiz-name' 
-                        value={name}
-                        onChange={(event) => setName(event.target.value)}/>
-                        <label for="floatingInput">Name</label>
-                    </div>
-                    <div class="form-floating">
-                        <input 
-                        type="password" 
-                        class="form-control" 
-                        placeholder='description' 
-                        value={description}
-                        onChange={(event) => setDescription(event.target.value)}/>
-                        <label for="floatingPassword">Description</label>
-                    </div>
-                    <div className='my-3'>
-                        <Select
-                            value={type}
-                            // onChange={this.handleChange}
-                            options={options}
-                            placeholder={'Quiz type ...'}
-                        />
-                    </div>
-                    <div className='more-actions form-group'>
-                        <label className='mb-1'>Upload Image</label>
-                        <input 
-                        type='file' 
-                        className='form-control' 
-                        onChange={(event) => handleChangeFile(event.target.value)}/>
-                    </div>
-                </fieldset>
-            </div>
-            <div className="list-detail">
-
-            </div>
-        </div>
-    )
-}
-
-export default ManageQuiz;
+    export default ManageQuiz;
