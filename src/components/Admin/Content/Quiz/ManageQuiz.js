d9a9ccc9fd30d1ffd9cbbe66ecfc2c7fe4d3cd25
Khoa Tran <khoatran3005@gmail.com>
2023-05-01 07:03:12 -0400

    last

--- src/components/Admin/Content/Quiz/ManageQuiz.js
+++ src/components/Admin/Content/Quiz/ManageQuiz.js
@@ -10,7 +10,9 @@ import ModalDeleteQuiz from './ModalDeleteQuiz';
 import { getAllQuizForAdmin } from "../../../../service/apiService"
 import QuizQA from './QuizQA';
 import AssignQuiz from './AssignQuiz';
-    import { useTranslation, Trans } from 'react-i18next';
+import { useTranslation, Trans } from 'react-i18next';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
 const options = [
     { value: 'EASY', label: 'EASY' },
@@ -25,7 +27,7 @@ const ManageQuiz = (props) => {
     const [type, setType] = useState('EASY');
     const [image, setImage] = useState('');
     const { t } = useTranslation();
-
+    const [key, setKey] = useState('profile');
     const [listQuiz, setListQuiz] = useState([])
 
     const [showEditModal, setShowEditModal] = useState(false);
@@ -80,77 +82,74 @@ const ManageQuiz = (props) => {
     }
     return (
         <div className="quiz-container">
-            <Accordion defaultActiveKey="0">
-                <Accordion.Item eventKey="0">
-                    <Accordion.Header>{t('quiz.m-quiz')}</Accordion.Header>
-                    <Accordion.Body>
-                        <div className="add-new">
-                            <fieldset className="border rounded-3 p-3">
-                                <legend className="float-none w-auto px-3">{t('quiz.add-quiz')}:</legend>
-                                <div className="form-floating mb-3">
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        placeholder='your-quiz-name'
-                                        value={name}
-                                        onChange={(event) => setName(event.target.value)} />
-                                    <label htmlFor="floatingInput">{t('quiz.name')}</label>
-                                </div>
-                                <div className="form-floating">
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        placeholder='description'
-                                        value={description}
-                                        onChange={(event) => setDescription(event.target.value)} />
-                                    <label htmlFor="floatingPassword">{t('quiz.des')}</label>
-                                </div>
-                                <div className='my-3'>
-                                    <Select
-                                        defaultValue={type}
-                                        onChange={setType}
-                                        // onChange={this.handleChange}
-                                        options={options}
-                                        placeholder={t('quiz.type')}
-                                    />
-                                </div>
-                                <div className='more-actions form-group'>
-                                    <label className='mb-1'>{t('quiz.up-img')}</label>
-                                    <input
-                                        type='file'
-                                        className='form-control'
-                                        onChange={(event) => handleChangeFile(event)} />
-                                </div>
-                                <div className='mt-3'>
-                                    <button
-                                        className='btn btn-warning '
-                                        onClick={() => handleSubmitQuiz()}>{t('quiz.save')}</button>
-                                </div>
-                            </fieldset>
-                        </div>
-                        <div className="list-detail">
-                            <TableQuiz
-                                listQuiz={listQuiz}
-                                fetchQuiz={fetchQuiz}
-                                handleClickEditQuiz={handleClickEditQuiz}
-                                handleClickDeleteQuiz={handleClickDeleteQuiz} />
-                        </div>
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="1">
-                    <Accordion.Header>{t('quiz.up-quiz')}</Accordion.Header>
-                    <Accordion.Body>
-                        <QuizQA/>
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="2">
-                    <Accordion.Header>{t('quiz.as-quiz')}</Accordion.Header>
-                    <Accordion.Body>
-                        <AssignQuiz/>
-                    </Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
+            <Tabs
+                id="controlled-tab-example"
+                activeKey={key}
+                onSelect={(k) => setKey(k)}
+                className="mb-3"
+                fill
+            >
+                <Tab eventKey="profile" title={t('quiz.m-quiz')}>
+                    <div className="add-new">
+                        <fieldset className="border rounded-3 p-3">
+                            <legend className="float-none w-auto px-3">{t('quiz.add-quiz')}:</legend>
+                            <div className="form-floating mb-3">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder='your-quiz-name'
+                                    value={name}
+                                    onChange={(event) => setName(event.target.value)} />
+                                <label htmlFor="floatingInput">{t('quiz.name')}</label>
+                            </div>
+                            <div className="form-floating">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder='description'
+                                    value={description}
+                                    onChange={(event) => setDescription(event.target.value)} />
+                                <label htmlFor="floatingPassword">{t('quiz.des')}</label>
+                            </div>
+                            <div className='my-3'>
+                                <Select
+                                    defaultValue={type}
+                                    onChange={setType}
+                                    // onChange={this.handleChange}
+                                    options={options}
+                                    placeholder={t('quiz.type')}
+                                />
+                            </div>
+                            <div className='more-actions form-group'>
+                                <label className='mb-1'>{t('quiz.up-img')}</label>
+                                <input
+                                    type='file'
+                                    className='form-control'
+                                    onChange={(event) => handleChangeFile(event)} />
+                            </div>
+                            <div className='mt-3'>
+                                <button
+                                    className='btn btn-warning '
+                                    onClick={() => handleSubmitQuiz()}>{t('quiz.save')}</button>
+                            </div>
+                        </fieldset>
+                    </div>
+                    <div className="list-detail">
+                        <TableQuiz
+                            listQuiz={listQuiz}
+                            fetchQuiz={fetchQuiz}
+                            handleClickEditQuiz={handleClickEditQuiz}
+                            handleClickDeleteQuiz={handleClickDeleteQuiz} />
+                    </div>
+                </Tab>
+                <Tab eventKey="password" title={t('quiz.up-quiz')}>
+                    <QuizQA />
+                </Tab>
+                <Tab eventKey="history" title={t('quiz.as-quiz')}>
+                    <AssignQuiz />
+                </Tab>
 
+            </Tabs>
 
             <ModalEditQuiz
                 dataQuizUpdate={dataQuizUpdate}
